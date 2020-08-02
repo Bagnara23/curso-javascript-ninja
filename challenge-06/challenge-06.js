c7bf82c47ae3589ae7986b3f5449f8ce8dd25f99
Bagnara23 <33769450+Bagnara23@users.noreply.github.com>
2020-08-02 18:21:00 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -1,23 +1,14 @@
-/*
-Vamos falar um pouco sobre "Futebol". Escolha um campeonato estadual qualquer
-para começar o desafio.
-Declare uma variável chamada `championship` que receberá o nome do campeonato,
-e imprima o nome desse campeonato no console.
-*/
-// ?
+/* Vamos falar um pouco sobre "Futebol". Escolha um campeonato estadual qualquer para começar o desafio.
+Declare uma variável chamada `championship` que receberá o nome do campeonato, e imprima o nome desse campeonato no console. */
+let championship = 'Gauchão 2020';
+console.log(championship); // Gauchão 2020
 
-/*
-Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
-Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
-estar na ordem em que eles aparecem na tabela no momento da solução desse
-desafio.
-*/
-// ?
+/* Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
+Os elementos serão nomes de times do campeonato escolhido, e os nomes devem estar na ordem em que eles aparecem na tabela no momento da solução desse desafio. */
+let teams = ['Gremio', 'Internacional', 'Juventude', 'Aimoré', 'Caxias'];
+console.log( 'Times que estão participando do campeonato:', teams ); // Times que estão participando do campeonato: (5) ["Gremio", "Internacional", "Juventude", "Aimoré", "Caxias"]
 
-console.log( 'Times que estão participando do campeonato:', teams );
-
-/*
-Crie uma função chamada `showTeamPosition` com as seguintes características:
+/* Crie uma função chamada `showTeamPosition` com as seguintes características:
     - A função deve receber um número por parâmetro;
     - A função deve retornar a frase:
     "O time que está em [POSIÇÃO]º lugar é o [NOME DO TIME].";
@@ -32,35 +23,92 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(campeonato, clubes, posicao) {
+    let classificacao = '';
+    if(posicao>0 && posicao <=5) {
+        classificacao = 'O time que está em ' + posicao + 'º lugar do campeonato ' + campeonato + ' é o ' + clubes[--posicao] + '.'; 
+    } else {
+        classificacao = 'Não temos a informação de time que está nessa posição.'; 
+    }
 
-/*
-Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
-função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
-*/
-// ?
+    return classificacao;
+}
 
-/*
-Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
-repetição "while".
-*/
-// ?
+showTeamPosition(championship, teams, 1); // O time que está em 1º lugar do campeonato Gauchão 2020 é o Gremio.
 
-/*
-Crie uma função chamada `convertToHex`, com as seguintes características:
+/* Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros. */
+showTeamPosition(championship, teams, 2); // O time que está em 2º lugar do campeonato Gauchão 2020 é o Internacional.
+showTeamPosition(championship, teams, 3); // O time que está em 3º lugar do campeonato Gauchão 2020 é o Juventude."
+showTeamPosition(championship, teams, 4); // O time que está em 4º lugar do campeonato Gauchão 2020 é o Aimoré.
+showTeamPosition(championship, teams, 5); // O time que está em 5º lugar do campeonato Gauchão 2020 é o Caxias.
+showTeamPosition(championship, teams, 20); // Não temos a informação de time que está nessa posição.
+
+/* Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de repetição "while". */
+let max = 50;
+let cont = 0;
+
+while(cont < max) {
+    if(cont >= 20 && cont <=30) {
+        console.log(cont);
+    }
+    cont++;
+}
+
+/* Crie uma função chamada `convertToHex`, com as seguintes características:
     - A função recebe uma cor por parâmetro, do tipo string. Exemplo: "red";
-    - Escolha 5 cores que serão convertidas do nome da cor para o seu
-    equivalente hexadecimal (pode ser qualquer tom);
-    - Usando a estrutura switch, verifique se a cor passada por parâmetro é
-    algum hexa escolhido. Se for, retorne a frase:
-    "O hexadecimal para a cor [COR] é [HEXADECIMAL].";
-    - Se a cor passada por parâmetro não estiver entre as selecionadas, mostre
-    a frase:
-    "Não temos o equivalente hexadecimal para [COR]."
+    - Escolha 5 cores que serão convertidas do nome da cor para o seu equivalente hexadecimal (pode ser qualquer tom);
+    - Usando a estrutura switch, verifique se a cor passada por parâmetro é algum hexa escolhido. Se for, retorne a frase: "O hexadecimal para a cor [COR] é [HEXADECIMAL].";
+    - Se a cor passada por parâmetro não estiver entre as selecionadas, mostre a frase: "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
 
-/*
-Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
-*/
-// ?
+function convertToHex(nameColor) { // Se usar o return nao precisa de break?
+    switch(nameColor) {
+        case 'vermelho':
+            return 'O hexadecimal para a cor ' + nameColor + ' é #FF0000';
+        case 'azul':
+            return 'O hexadecimal para a cor ' + nameColor + ' é #0000FF';
+        case 'verde':
+            return 'O hexadecimal para a cor ' + nameColor + ' é #00FF00';
+        case 'amarelo':
+            return 'O hexadecimal para a cor ' + nameColor + ' é #FFFF00';
+        case 'rosa':
+            return 'O hexadecimal para a cor ' + nameColor + ' é #FF1493';
+        default:
+            return 'Não temos o equivalente hexadecimal para ' + nameColor + '.';
+    }
+}
+
+function convertToHex(nameColor) { // se atribuir o retorno a uma variavel é necessario o break. 
+    let color = '';
+    switch(nameColor) {
+        case 'vermelho':
+            color =  'O hexadecimal para a cor ' + nameColor + ' é #FF0000';
+            break;
+        case 'azul':
+            color =  'O hexadecimal para a cor ' + nameColor + ' é ##0000FF';
+            break;
+        case 'verde':
+            color =  'O hexadecimal para a cor ' + nameColor + ' é #00FF00';
+            break;
+        case 'amarelo':
+            color =  'O hexadecimal para a cor ' + nameColor + ' é #FFFF00';
+            break;
+        case 'rosa':
+            color =  'O hexadecimal para a cor ' + nameColor + ' é #FF1493';
+            break;
+        default:
+            color =  'Não temos o equivalente hexadecimal para ' + nameColor + '.';
+    }
+
+    return color;
+}
+
+/* Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima. */
+convertToHex('vermelho'); // O hexadecimal para a cor vermelho é #FF0000
+convertToHex('verde'); // O hexadecimal para a cor verde é #00FF00
+convertToHex('azul'); // O hexadecimal para a cor azul é #0000FF
+convertToHex('marrom'); // Não temos o equivalente hexadecimal para marrom.
+convertToHex('preto'); // Não temos o equivalente hexadecimal para preto.
+convertToHex('amarelo'); // O hexadecimal para a cor amarelo é #FFFF00
+convertToHex('rosa'); // O hexadecimal para a cor rosa é #FF1493
+convertToHex('cinza'); // Não temos o equivalente hexadecimal para cinza.
